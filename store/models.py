c49700273a8aa6d34a62ab484a9ab43bbe058615
Deepa Rajkumar <deeparajkumar59@gamil.com>
2022-10-10 00:39:43 +0530

    product gallery

--- store/models.py
+++ store/models.py
@@ -31,7 +31,7 @@ class Product(models.Model):
         count = 0
         if reviews['count'] is not None:
             count = int(reviews['count'])
-        return count    
+        return count
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager,self).filter(variation_category='color',is_active=True)
@@ -65,9 +65,15 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product,default=None, on_delete=models.CASCADE)
+    image =models.ImageField(upload_to='store/products',max_length=255)
 
-
-
+    def __str__(self):
+        return self.product.product_name
+    class Meta:
+        verbose_name="productgallery"
+        verbose_name_plural='product gallery'
 
 
 
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404, redirect
-from .models import Product, ReviewRating
+from .models import Product, ReviewRating,ProductGallery
 from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
@@ -49,11 +49,14 @@ def product_detail(request,category_slug,product_slug):
 
     # Get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+    #Get the product gallary
+    product_gallary = ProductGallery.objects.filter(product_id=single_product.id)
     context={
     'single_product':single_product,
     'in_cart':in_cart,
     'orderproduct': orderproduct,
     'reviews': reviews,
+    'product_gallary':product_gallary
     }
     return render(request,'store/product_detail.html',context)
 def search(request):
